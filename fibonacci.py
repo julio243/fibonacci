85976ec3b1255f95cd213853efd232786af2aa85
julio243 <fernandoflo993@gmail.com>
2020-06-10 10:56:52 -0500

    fib2

--- fibonacci.py
+++ fibonacci.py
@@ -4,4 +4,12 @@ def fib(n):
     while a < n:
         print(a, end=' ')        
         a, b = b, a+b
-    print()
+    print("fib1")
+    
+def fib2(n):
+    """Print a Fibonacci series up to n."""
+    a, b = 0, 1
+    while a < n:
+        print(a, end=' ')        
+        a, b = b, a+b
+    print("fib2")
